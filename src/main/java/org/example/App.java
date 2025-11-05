11d1b1db577a365d24d4677ff6844477d5021c58
kreeeks <giannis.krikis50@gmail.com>
2025-11-05 19:55:23 +0200

    Project start

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,35 @@
+package org.example;
+import java.util.Scanner;
+
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        Scanner scanner =new Scanner(System.in);
+        System.out.print( "ΔΩΣΕ ΠΡΩΤΟ ΑΡΙΘΜΟ" );
+        double num1 = scanner.nextDouble();
+        System.out.print("ΔΩΣΕ ΠΡΑΞΗ (+,-,/,*) ");
+        String operation = scanner.next();
+        System.out.print("ΔΩΣΕ ΔΕΥΤΕΡΟ ΑΡΙΘΜΟ");
+        double num2 = scanner.nextDouble();
+        scanner.close();
+
+        if (operation.equals("+")) {
+
+        }
+        else if (operation.equals("-")) {
+
+        }
+        else if (operation.equals("*")) {
+
+        }
+        else if (operation.equals("/")) {
+
+        }
+        else {
+            System.out.print("ΑΓΝΩΣΤΗ ΠΡΑΞΗ ");
+        }
+
+    }
+}
